5764a55a73f7af1b269c61e38c61b409a2716664
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-27 22:59:20 +0100

    the header file

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include<string.h>
+#include<unistd.h>
+
+
+
+
+
+#endif
