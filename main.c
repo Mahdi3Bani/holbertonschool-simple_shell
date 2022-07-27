5e8c2445695abf474204390938b8149263823d3c
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-27 23:08:47 +0100

    this is the boss

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+/**
+ * main - a simple shell (gates of s"hell" r open)
+ * @argc: Argument Count
+ * @argv:Argument Value
+ * Return: idk yet
+ */
+int main(int argc, char **argv)
+{
+
+printf("idk what to do :)\n");
+
+
+
+}
