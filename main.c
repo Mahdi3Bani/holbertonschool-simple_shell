917aad93f24280525b0bb9af5beea8084e87a456
imenmansouri93 <imennmansourii@gmail.com>
2022-07-28 23:51:13 +0100

    mainfunction

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include "shell.h"
 /**
  * main - a simple shell (gates of s"hell" r open)
@@ -7,9 +11,24 @@
  */
 int main(int argc, char **argv)
 {
+ char *cmd;
+ do
+ {
+  print_prompt1();
+  cmd = read_cmd();
+  if (!cmd)
+  {
+  exit(EXIT_SUCCESS);
+  }
+  if (cmd[0] == '\0' || strcmp(cmd, "\n") == 0 )
+  {
+  free(cmd);
+  break;
+  }
+  printf("%s\n", cmd);
 
-printf("idk what to do :)\n");
-
-
-
+  free(cmd);
+ } while (1);
+ 
+exit(EXIT_SUCCESS);
 }
