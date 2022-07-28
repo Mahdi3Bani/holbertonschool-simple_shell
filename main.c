9a52d1cf58914165784e848ae99c075490b534f7
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-28 23:58:23 +0100

    main

--- main.c
+++ main.c
@@ -8,8 +8,31 @@
 int main(int argc, char **argv)
 {
 
-printf("idk what to do :)\n");
+char *cmd;
 
+while (1)
+{
+    print_1();
+    cmd = read_cmd();
 
+    if (!cmd)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    if (cmd [0] == '\0' || strcmp(cmd, "\n") == 0)
+    {
+        free(cmd);
+        continue;
+    }
+     if(strcmp(cmd, "exit\n") == 0)
+        {
+            free(cmd);
+            break;
+        }
+        printf("%s\n", cmd);
+        free(cmd);
+}
+
+    exit(EXIT_SUCCESS);
 
 }
