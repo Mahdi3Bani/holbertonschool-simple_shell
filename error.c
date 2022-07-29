b78b517d7bf46b59fbc42bf5d46a3c919aee1c97
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-30 00:48:00 +0100

    w bara 3adi kn ghlotet w wino lmochkel

---
+++ error.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+ *_error - prints error msg
+ *@l: number line
+ *@argument: arg :)
+ *@str: what make the error ,)
+ */
+
+void _error(int l, char **argument, char *str)
+{
+	write(STDERR_FILENO, argument[0], strlen(argument[0]));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, uitoa(l), strlen(uitoa(l)));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, str, strlen(str));
+	write(STDERR_FILENO, ": not found\n", 12);
+}
