54ec7123f3b6ded41c0aad45a5beed12bb579f5e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-30 01:57:09 +0100

    just parsing

---
+++ parse.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+/**
+ *parse - just parsing
+ *@argument: the command
+ *Return: command line
+ */
+char *parse(char **argument)
+{
+	int i;
+
+	if (argument[0] == "exist")
+	{
+		clear(argument);
+		exit(EXIT_SUCCESS);
+	}
+	else if (argument[0] == "env")
+	{
+		for (i = 0; env[i]; i++)
+		{
+			write(STDOUT_FILENO, env[i], strlen(env[i]));
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		clear(argument);
+		return (NULL);
+	}
+	else
+		return (argument[0]);
+}
