f8940f17b59a9e798fe2a4536234e3d12fa2955b
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-07-30 17:00:11 +0100

    3abi 3abi

---
+++ stock.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+/**
+ * stock - storing args
+ * @buff: command line
+ * @delimeters: list of char
+ * Return: array of argument
+ */
+char **stock(buff, delimiters)
+{
+	char **args, *token, *str;
+	int nb, i = 0;
+
+	nb = count_args(buff, ' ');
+	token = strtok(buff, delimiters);
+	args = malloc(sizeof(char *) * (nb + 1));
+	args[i] = strdup(token);
+	while (token && ++i)
+	{
+		token = strtok(NULL, delimiters);
+		if (token)
+		{
+			args[i] = strdup(token);
+		}
+	}
+	args[i] = NULL;
+	if (count_args(args[0], '/') > 1)
+	{
+		token = strtok(args[0], "/");
+		while (token)
+		{
+			str = strdup(token);
+			token = strtok(NULL, "/");
+			if (token)
+			{
+				free(str), str = NULL;
+			}
+		}
+		free(args[0]), args[0] = NULL;
+		args[0] = strdup(str);
+		free(str), str = NULL;
+	}
+	free(buff), buff = NULL;
+	return (args);
+}
